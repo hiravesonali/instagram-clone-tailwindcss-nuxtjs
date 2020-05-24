ae5268e50de8e689032cf5a58297d3b417857a6f
Sonali Hirave <hiravesonali@gmail.com>
2020-05-24 14:49:26 +0200

    comments add

--- pages/index.vue
+++ pages/index.vue
@@ -69,6 +69,7 @@
     </nav>
 
           <!--body start-->
+          <!--profile data-->
     <div class="bg-gray-100 h-auto px-48">
 
       <div class="flex md:flex-row-reverse flex-wrap">
@@ -164,7 +165,7 @@
       <hr class="border-gray-500 mt-6">
       <hr class="border-gray-500 w-20 border-t-1 ml-64 border-gray-800">
 
-
+            <!--post icon and title-->
             <div class="flex flex-row mt-4 justify-center mr-16">
               <div class="flex text-gray-700 text-center py-2 m-2 pr-5">
                 <div class="flex inline-flex">
@@ -219,7 +220,7 @@
               </div>
           </div>
 
-
+              <!--post images-->
             <div class="flex pt-4">
               <div class="flex-1 text-center px-4 py-2 m-2">
                 <img class="w-full" src="https://lh3.googleusercontent.com/RPc_sH0A6HJBobZ894_twD3LYoTRN8tHQCRaoMlGIpRt1eShcViax5rFfWSrdHyQUiyDRI9qaA_pB6eSuBjTvkY-S4gJh5av6xC7b7aa-O8WzQKDtn2AtbcXmmADTlhGX77_eA8ki85SWI65eWg4VxJeog9HSsEf2qaBaEeDqZWbU4HPBQ7Y0cDdh9CR8kVnrGPRh1VlBWkC6srT5pnd2PgOn0fI7snrcJbG611fBS1tirkVwCKSPeyu8IVcdfrtZxvM4n1emgf9ynQ9N2RyMSYvkMKlyrTaZP7qkOL4CQODq9ikzXJYqCl3PtlaWCbvrnXiFytJofv4r-npNyL8505Fq-pxTX9A4rDiIM8uIgRU3OLoFCSax8v_y3DhXq_J6x8RpWWAKLm-W-p_UvG4d9tvCI_3AWp7nIncK_TADThP9etMeQNlSeZsm3ACdWbfQxaNKNTbh3jO_GgFMo-HHSEKwcoaK4YBarQ7iP2gWhu1l7qynAj_O6Y_ccL5Snvy_rmssBDZNVwO63FE0Hz8USdPHstei05uy8UyacXxnSX1rPUqNQuTm1Ja6wHQhiryP1w5PxXZ23xx5ZXt9s-0_QPzTvFbNLwy5IdYEjx7eK_0PTsnRGsGcdU61Q6laMqR_tAOkijM0tB0qo2j_nfJOLySqCtmbac_851A1RJYcNEib_WQRESlO8sQS5F8=w864-h766-no?authuser=0">
